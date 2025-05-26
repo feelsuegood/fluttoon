0eeaf0c6ae0f15acc9811b0b470d361e2dee9c0a
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-26 16:51:08 +1000

    webtoon card - clipBehavior, Image.network, boxShadow

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -29,32 +29,74 @@ class HomeScreen extends StatelessWidget {
           if (snapshot.hasData) {
             //*for debug
             // return const Text("There is data");
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                // only load items on the screen
-                // print(index);
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => SizedBox(width: 20),
+            return Column(
+              children: [
+                SizedBox(height: 20),
+                Expanded(child: makeList(snapshot)),
+              ],
             );
           }
-          // return ListView.builder(
-          //     scrollDirection: Axis.horizontal,
-          //     itemCount: snapshot.data!.length,
-          //     itemBuilder: (context, index) {
-          //*       // only load items on the screen
-          //*      // print(index);
-          //       var webtoon = snapshot.data![index];
-          //       return Text(webtoon.title);
-          //     },
-          //   );
+
           return Center(child: CircularProgressIndicator());
         },
       ),
     );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        // only load items on the screen
+        // print(index);
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              //* remeber when you try border radious
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    // how far shadow is
+                    blurRadius: 15,
+                    // choose where is the sun
+                    offset: Offset(10, 15),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {'Referer': 'https://comic.naver.com'},
+                //* doesn't work
+                // headers: {
+                //   "User-Agent":
+                //       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                // },
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(webtoon.title, style: TextStyle(fontSize: 22)),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(width: 40),
+    );
+    // return ListView.builder(
+    //     scrollDirection: Axis.horizontal,
+    //     itemCount: snapshot.data!.length,
+    //     itemBuilder: (context, index) {
+    //*       // only load items on the screen
+    //*      // print(index);
+    //       var webtoon = snapshot.data![index];
+    //       return Text(webtoon.title);
+    //     },
+    //   );
   }
 }
 

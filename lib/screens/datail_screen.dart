b28bd81c4d4d40520cc2e5ef810ec80c7fbb5c21
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-26 18:04:49 +1000

    navigator, detail screen

---
+++ lib/screens/datail_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 4,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.white,
+        foregroundColor: Color.fromARGB(255, 117, 0, 200),
+        surfaceTintColor: Colors.transparent,
+        title: Text(
+          title,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 250,
+                //* remeber when you try border radious
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      // how far shadow is
+                      blurRadius: 15,
+                      // choose where is the sun
+                      offset: Offset(10, 15),
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                  ],
+                ),
+                child: Image.network(
+                  thumb,
+                  headers: const {'Referer': 'https://comic.naver.com'},
+                  //* doesn't work
+                  // headers: {
+                  //   "User-Agent":
+                  //       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                  // },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttoon/models/webtoon_model.dart';
 import 'package:fluttoon/seervices/api_service.dart';
+import 'package:fluttoon/widgets/webtoon_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -19,9 +20,12 @@ class HomeScreen extends StatelessWidget {
         elevation: 4,
         shadowColor: Colors.black,
         backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
+        foregroundColor: const Color.fromARGB(255, 117, 0, 200),
         surfaceTintColor: Colors.transparent,
-        title: Text("Webtoons for Today", style: TextStyle(fontSize: 24)),
+        title: Text(
+          "Webtoons for Today",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        ),
       ),
       body: FutureBuilder(
         future: webtoons,
@@ -52,37 +56,10 @@ class HomeScreen extends StatelessWidget {
         // only load items on the screen
         // print(index);
         var webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              width: 250,
-              //* remeber when you try border radious
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    // how far shadow is
-                    blurRadius: 15,
-                    // choose where is the sun
-                    offset: Offset(10, 15),
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                ],
-              ),
-              child: Image.network(
-                webtoon.thumb,
-                headers: const {'Referer': 'https://comic.naver.com'},
-                //* doesn't work
-                // headers: {
-                //   "User-Agent":
-                //       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                // },
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(webtoon.title, style: TextStyle(fontSize: 22)),
-          ],
+        return Webtoon(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
         );
       },
       separatorBuilder: (context, index) => SizedBox(width: 40),
